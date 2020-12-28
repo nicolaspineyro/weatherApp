29d6ac7d1b80382e75e6ccf78f9347e8c7ca1fe5
Nicolas Piñeyro <nicolaspineyro@users.noreply.github.com>
2020-12-28 18:49:25 -0300

    Initializing

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <Text>Hola Mundo</Text>
+    </>
+  );
+};
+
+
+export default App;
